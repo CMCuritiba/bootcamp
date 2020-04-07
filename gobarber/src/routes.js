aee838cfdc7569d395a1976fb176759a6762d032
Deini Porto <dasporto@yahoo.com.br>
2020-04-07 11:41:05 -0300

    final do Listando horários disponíveis.

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -11,6 +11,7 @@ import NotificationController from './app/controllers/NotificationController';
 
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -25,6 +26,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
